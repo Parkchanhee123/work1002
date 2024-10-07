ee15911ce69f8441afe1de45bbc41074be8986d3
chPark <pchh213@naver.com>
2024-10-07 09:45:02 +0900

    cust, item, review, Totalorder 작업 완료

--- service/src/test/java/review/C211_2.java
+++ service/src/test/java/review/C211_2.java
@@ -2,16 +2,27 @@ package review;
 
 import edu.sm.dto.Review;
 import edu.sm.service.ReviewService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class C211_2 {
     public static void main(String[] args) {
-        ReviewService reviewService = new ReviewService();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ReviewService reviewService = (ReviewService) context.getBean("reviewService");
+
         Review review = Review.builder()
                 .review_id(1)
                 .build();
+
         try {
-            reviewService.C211_2(review);
+            boolean result = reviewService.C211_2(review);
+            if (result) {
+                System.out.println("Review deleted successfully.");
+            } else {
+                System.out.println("Review deletion failed.");
+            }
         } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
